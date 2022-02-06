d6741f1d744ce5971ff0ccba212ab43970905ef7
yejin <dlekrn0528@naver.com>
2022-02-07 01:47:27 +0900

    feat: Post 모델에 img, file 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+    head_img = models.ImageField(upload_to="blog/images/%Y/%m/%d/", blank=True)
+    file_uploead = models.FileField(upload_to="blog/files/%Y/%m/%d/", blank=True)
 
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
